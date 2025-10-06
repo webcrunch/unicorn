456c4422884a7a0b90fb9fe9c4d5f648697270c9
Nehaasati <31asatineha@gmail.com>
2025-10-06 12:16:54 +0200

    Add constuctor

--- user.cs
+++ user.cs
@@ -8,6 +8,17 @@ namespace App
         public string Username;
         public string Password;
         public List<Item> MyItems;        // User can have many items
-        public List<Trade> MyTrades;
+        public List<Trade> MyTrades;      // Trades involving this user
+
+        // Constructor to initialize user
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+            MyItems = new List<Item>();
+            MyTrades = new List<Trade>();
+        }
     }
 }
+
+  
