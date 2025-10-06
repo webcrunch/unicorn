a757e627e4d106570e71c6c698e2712985b91ae1
FatimaAlmurtadha <sukaina77150@gmail.com>
2025-10-06 15:05:51 +0200

    Creat an info method, in order to write out all item's information

--- Item.cs
+++ Item.cs
@@ -6,12 +6,18 @@ class Item
   public string? Description;
   public int Amount;
 
-  public string owner;
+  public string Owner;
 
-  public Item(string? name, string? description, int amount,string owner)
+  public Item(string? name, string? description, int amount, string owner)
   {
     Name = name;
     Description = description;
     Amount = amount;
+    Owner = owner;
+  }
+
+  public void Info()
+  {
+    Console.WriteLine($"The item's name is: {Name}, \ndescription: {Description}, \namount: {Amount} \nand owner is: {Owner}. ");
   }
 }
