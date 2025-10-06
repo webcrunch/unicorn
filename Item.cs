7b07ce7e8d04b5d77b1b7df2229c5139b09482ff
FatimaAlmurtadha <sukaina77150@gmail.com>
2025-10-06 11:45:12 +0200

    Creat a constructor of the class

--- Item.cs
+++ Item.cs
@@ -5,4 +5,11 @@ class Item
   public string? Name;
   public string? Description;
   public int Amount;
+
+  public Item(string? name, string? description, int amount)
+  {
+    Name = name;
+    Description = description;
+    Amount = amount;
+  }
 }
