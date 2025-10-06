b595eb2031fe592f780903b6637bc90316168056
FatimaAlmurtadha <sukaina77150@gmail.com>
2025-10-06 14:36:30 +0200

    Creat a constructor

--- Item.cs
+++ Item.cs
@@ -5,4 +5,13 @@ class Item
   public string? Name;
   public string? Description;
   public int Amount;
+
+  public Item(string? name, string? description, int amount)
+
+  {
+    Name = name;
+    Description = description;
+    Amount = amount;
+
+  }
 }
